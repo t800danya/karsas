b8c38f3da00d43368fe34746ccb16d814c32c07c
t800danya <t800@kvkozyrev.org>
2017-09-04 22:20:36 +0600

    Create karkas.h

---
+++ karkas.h
@@ -0,0 +1,21 @@
+/********************************************************************************
+
+ *   Copyright (C) 2017 by Bogdan Kozyrev <t800@kvkozyrev.org>                  *
+
+ *   All rights reserved.                                                       *
+
+ *******************************************************************************/
+
+
+int Hello();
+bool Yes_No();
+int PressEnter();
+int PressAnyKey();
+int Timer();
+int GenerateA();
+int GetAllMassives();
+int Calculate_b_and_v();
+int Calculate_g();
+int WorkWithMassiveB2();
+int Start();
+int Continue();
